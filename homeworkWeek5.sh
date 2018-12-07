604edde82e65c70b9bb2f6db361556fa363b3d6f
jembrown <jeremymbrown@gmail.com>
2018-12-07 12:09:35 -0600

    Comments added for week 5 homework

--- homeworkWeek5.sh
+++ homeworkWeek5.sh
@@ -8,8 +8,12 @@ for myWordLength in cakes cookies desserts ice\cream
 do
        if [ $myWordLength = cookies ]
        then
-            echo $myWordLength "is the same length as cookies."
+            echo $myWordLength "is the same as cookies."
        else
-            echo $myWordLength "is a different length than cookies."
+            echo $myWordLength "is different than cookies."
        fi
 done
+
+# DB: Good! But you're not actually using the command-line arguments at all. They're
+#     overwritten by the words you're looping over in your for loop. Also, you're testing
+#     whether the words are identical, but not actually their lengths.
